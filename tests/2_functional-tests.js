39e3fadac8cda61e3bc443425fcd68b8c5d60876
Diego Da'Silva <smartcrashomg@gmail.com>
2021-09-18 10:55:59 -0400

    Formatting

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,8 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
-const assert = chai.assert;
-const server = require('../server');
+const chaiHttp = require('chai-http')
+const chai = require('chai')
+const assert = chai.assert
+const server = require('../server')
 
-chai.use(chaiHttp);
+chai.use(chaiHttp)
 
-suite('Functional Tests', function() {
-  
-});
+suite('Functional Tests', function () {})
